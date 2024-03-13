37e21a0599eb832986146d61cd0c554618dfbced
CalebRdgz <calebrodgz@gmail.com>
2024-03-13 16:32:22 -0500

    initialized header component

---
+++ store/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //target all the files that end with .html and .ts in the src folder:
+  content: ["./src/**/*.(html, ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
